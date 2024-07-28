b7c0fd00d128c6db05c13b4195d8c8f60350efd6
Alexius Strauss <alexius@MacBookPro.local>
2024-07-28 03:41:35 -0300

    Criacao da estrutura da API no DRF e criacao dos recursos de Customers

--- backend/src/apps/customers/models.py
+++ backend/src/apps/customers/models.py
@@ -2,6 +2,22 @@ from django.db import models
 
 
 class Customer(models.Model):
+    """
+    Modelo para representar um cliente.
+
+    Campos:
+        - name (str): Nome completo do cliente.
+        - email (str): Email do cliente único e obrigatório.
+        - created_at (datetime): Data de criação da conta.
+
+    Atributos especiais:
+        - __str__: Retorna a representação em string do nome do cliente.
+
+    Meta-dados:
+        - verbose_name: Nome singular do modelo para uso em formulários e listagens.
+        - verbose_name_plural: Nome plural do modelo para uso em formulários e listagens.
+    """
+
     name = models.CharField(verbose_name="Nome completo", max_length=120, blank=False)
     email = models.EmailField(verbose_name="Email", blank=False, unique=True)
     created_at = models.DateTimeField(auto_created=True, auto_now_add=True)
@@ -10,11 +26,33 @@ class Customer(models.Model):
         return str(self.name)
 
     class Meta:
+        """
+        Meta-dados do modelo Cliente.
+
+        Nome singular e plural para uso em formulários e listagens.
+        """
+
         verbose_name = "Cliente"
         verbose_name_plural = "Clientes"
 
 
 class Partner(models.Model):
+    """
+    Modelo para representar um parceiro.
+
+    Campos:
+        - customer (OneToOneField): O cliente associado a esse parceiro.
+        - activate (bool): Indica se o parceiro está ativo ou não.
+        - create_at (datetime): Data de criação da conta do parceiro.
+
+    Atributos especiais:
+        - __str__: Retorna uma string que representa o nome do parceiro e o estado de atividade.
+
+    Meta-dados:
+        - verbose_name: Nome singular do modelo para uso em formulários e listagens.
+        - verbose_name_plural: Nome plural do modelo para uso em formulários e listagens.
+    """
+
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, related_name='partner')
     activate = models.BooleanField(default=False)
     create_at = models.DateTimeField(auto_created=True, auto_now_add=True)
@@ -23,5 +61,11 @@ class Partner(models.Model):
         return f"{self.customer.name} - Active: {self.activate}"
 
     class Meta:
+        """
+        Meta-dados do modelo Parceiro.
+
+        Nome singular e plural para uso em formulários e listagens.
+        """
+
         verbose_name = "Parceiro"
         verbose_name_plural = "Parceiros"
--- backend/src/routes/urls.py
+++ backend/src/routes/urls.py
@@ -1,23 +1,54 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import render
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
+
+
+def error_404(request, exception):
+    return render(request, "error/404.html", status=404)
+
+
+def error_500(request):
+    return render(request, "error/500.html", status=500)
+
+
+handler404 = "src.routes.urls.error_404"
+handler500 = "src.routes.urls.error_500"
+
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=True):
+        schema = super().get_schema(request, public)
+        schema.schemes = settings.REST_FRAMEWORK_SCHEMAS
+        return schema
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="OrderManagerAPI",
+        default_version="v1",
+        description="API RESTful utilizando o Django Rest Framework (DRF)",
+        contact=openapi.Contact(email="alexius.dev@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    generator_class=BothHttpAndHttpsSchemaGenerator,
+)
+
+router = routers.SimpleRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('src.apps.customers.api.urls')),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
